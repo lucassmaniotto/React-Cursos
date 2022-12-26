dcf220927c76db698aa28e52589e3c54359bfebe
Lucas Smaniotto <lucas91755@gmail.com>
2022-12-26 08:14:04 -0300

    Criando o primeiro componente

---
+++ ReactJS/organo/src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+import Banner from './components/Banner/Banner';
+
+function App() {
+  return (
+    <div className="App">
+      <Banner />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Howdy partner yeeeehawwww! 🤠
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
