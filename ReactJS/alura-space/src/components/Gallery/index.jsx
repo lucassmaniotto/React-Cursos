3be1accab6f552fbd38add69b8c2b0dc03014096
Lucas Smaniotto <lucas91755@gmail.com>
2023-01-10 13:44:45 -0300

    feat: filtragem de cards por tags

--- ReactJS/alura-space/src/components/Gallery/index.jsx
+++ ReactJS/alura-space/src/components/Gallery/index.jsx
@@ -1,18 +1,33 @@
-import React from 'react';
+import { useState } from 'react';
 import Tags from '../Tags';
 import styles from './Gallery.module.scss';
 import Cards from './Cards';
 import photos from './photos.json';
 
 export default function Gallery() {
+  const [itens, setItens] = useState(photos);
+  const tags = [...new Set(photos.map((value) => value.tag))];
+
+  const filterPhotos = (tag) => {
+    const filteredPhotos = photos.filter((value) => {
+      return value.tag === tag; 
+    });
+
+    setItens(filteredPhotos);
+  };
+
   return (
     <section className={styles.gallery}>
         <h2>Navegue pela Galeria</h2>
-        <Tags />
+        <Tags 
+          tags={tags}
+          filterPhotos={filterPhotos}
+          setItens={setItens}
+        />
         <Cards 
-            photos={photos}
+            photos={itens}
             styles={styles}
         />
     </section>
-  )
+  );
 }
--- ReactJS/alura-space/src/components/Tags/index.jsx
+++ ReactJS/alura-space/src/components/Tags/index.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import styles from './Tags.module.scss';
+import photos from '../Gallery/photos.json';
 
-export default function Tags() {
+export default function Tags({ tags, filterPhotos, setItens }) {
   return (
     <div className={styles.tags}>
         <p>Busque por tags:</p>
         <ul className={styles.tags__list}>
-            <li>Estrelas</li>
-            <li>Galáxias</li>
-            <li>Luas</li>
-            <li>Planetas</li>
+          {tags.map((tag) => {
+            return <li key={tag} onClick={ () => filterPhotos(tag)}>{tag}</li>;
+          })}
+          <li onClick={() => setItens(photos)}>Todas</li>
         </ul>
     </div>
   )
