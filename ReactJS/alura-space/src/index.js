926b4305b5be63346d86299648d743fdc37435a1
Lucas Smaniotto <lucas91755@gmail.com>
2023-01-10 09:30:35 -0300

    style: melhorando a organização do scss com variáveis e importando fontes

--- ReactJS/alura-space/src/index.js
+++ ReactJS/alura-space/src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import "./assets/fonts/GandhiSans-Regular.otf"
+import "./assets/fonts/GandhiSans-Bold.otf"
+
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
