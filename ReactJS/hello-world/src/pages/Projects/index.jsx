edb9f65b4b42486d16c6a7e2d267040a2d0d7560
Lucas Smaniotto <lucas91755@gmail.com>
2023-01-13 13:29:06 -0300

    feat: implementação de recomendação de projetos

--- ReactJS/hello-world/src/pages/Projects/index.jsx
+++ ReactJS/hello-world/src/pages/Projects/index.jsx
@@ -1,10 +1,14 @@
 import { Route, Routes, useParams } from "react-router-dom"
 import posts from "json/posts.json"
-import PagesModel from "components/PagesModel";
 import ReactMarkdown from "react-markdown";
+
 import './Projects.css'
+import styles from './Projects.module.css'
+
+import PagesModel from "components/PagesModel";
 import NotFound from "pages/NotFound";
 import DefaultPage from "components/DefaultPage";
+import Card from "components/Card";
 
 export default function Projects() {
   const params = useParams();
@@ -16,6 +20,11 @@ export default function Projects() {
   if (!post) {
     return <NotFound />
   }
+
+  const recommendedProjects = posts
+    .filter((post) => post.id !== Number(params.id))
+    .sort((a, b) => b.id - a.id)
+    .slice(0, 4);
   
   return (
     <Routes>
@@ -30,6 +39,14 @@ export default function Projects() {
               {post.text}
             </ReactMarkdown>
           </div>
+          <h2 className={styles.titleOtherProjects}>Outros projetos que você pode gostar:</h2>
+          <ul className={styles.recommendedProjects}>
+            {recommendedProjects.map((post) => (
+              <li key={post.id}>
+                <Card post={post} />
+              </li>
+            ))}
+          </ul>
         </PagesModel>
       } />
       </Route>
