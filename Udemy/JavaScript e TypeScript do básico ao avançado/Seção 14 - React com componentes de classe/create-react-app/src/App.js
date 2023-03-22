25776fb14222b38c9e810485a99c6d6f85c9e661
Lucas Smaniotto <lucas91755@gmail.com>
2023-03-22 14:13:22 -0300

    chore: configurando projeto react

--- Udemy/JavaScript e TypeScript do básico ao avançado/Seção 14 - React com componentes de classe/create-react-app/src/App.js
+++ Udemy/JavaScript e TypeScript do básico ao avançado/Seção 14 - React com componentes de classe/create-react-app/src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
+import React from 'react';
+
 import './App.css';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <main>
+        <h1>Hello World!</h1>
+      </main>
+    </>
   );
 }
-
-export default App;
