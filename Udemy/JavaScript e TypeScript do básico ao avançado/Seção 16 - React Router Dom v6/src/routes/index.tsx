e83642cbc8ea584c165a46dab380e18f9c138da4
Lucas Smaniotto <101435037+lucassmaniotto@users.noreply.github.com>
2023-08-11 17:19:46 -0300

    feat: implementando rotas com React Router Dom v6

---
+++ Udemy/JavaScript e TypeScript do básico ao avançado/Seção 16 - React Router Dom v6/src/routes/index.tsx
@@ -0,0 +1,15 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import { Home } from "../pages/Home";
+import { About } from "../pages/About";
+
+export function AppRoutes() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
