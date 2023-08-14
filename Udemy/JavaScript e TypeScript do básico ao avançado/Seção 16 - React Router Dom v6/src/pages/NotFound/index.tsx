38336b46568e212fe96997142fa790557e27cb12
Lucas Smaniotto <101435037+lucassmaniotto@users.noreply.github.com>
2023-08-14 08:23:27 -0300

    feat: componente NotFound

---
+++ Udemy/JavaScript e TypeScript do básico ao avançado/Seção 16 - React Router Dom v6/src/pages/NotFound/index.tsx
@@ -0,0 +1,16 @@
+import { useNavigate } from "react-router-dom";
+
+export const NotFound = () => {
+  const navigate = useNavigate();
+
+  const handleNavigate = () => {
+    navigate(-1);
+  };
+  return (
+    <div>
+      <h1>404</h1>
+      <p>Page not found</p>
+      <button onClick={handleNavigate}>Go back</button>
+    </div>
+  );
+};
--- Udemy/JavaScript e TypeScript do básico ao avançado/Seção 16 - React Router Dom v6/src/routes/index.tsx
+++ Udemy/JavaScript e TypeScript do básico ao avançado/Seção 16 - React Router Dom v6/src/routes/index.tsx
@@ -4,6 +4,7 @@ import { Home } from "../pages/Home";
 import { About } from "../pages/About";
 import { DefaultPage } from "../pages/DefaultPage/indext";
 import { Post } from "../pages/Post";
+import { NotFound } from "../pages/NotFound";
 
 export function AppRoutes() {
   return (
@@ -14,7 +15,7 @@ export function AppRoutes() {
           <Route path="/about" element={<About />} />
           <Route path="/post" element={<Post />} />
           <Route path="/post/:id" element={<Post />} />
-          <Route path="*" element={<h1>Not Found</h1>} />
+          <Route path="*" element={<NotFound />} />
         </Route>
       </Routes>
     </BrowserRouter>
