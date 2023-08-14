d2965eb11174406c8de38b49cfa4b013655fb020
Lucas Smaniotto <101435037+lucassmaniotto@users.noreply.github.com>
2023-08-14 08:19:22 -0300

    feat: Redirect com useNavigate

--- Udemy/JavaScript e TypeScript do básico ao avançado/Seção 16 - React Router Dom v6/src/pages/About/index.tsx
+++ Udemy/JavaScript e TypeScript do básico ao avançado/Seção 16 - React Router Dom v6/src/pages/About/index.tsx
@@ -1,7 +1,15 @@
+import { useNavigate } from "react-router-dom";
+
 export const About = () => {
+  const navigate = useNavigate();
+
+  const handleNavigate = () => {
+    navigate(-1);
+  };
   return (
     <div>
       <h1>About</h1>
+      <button onClick={handleNavigate}>Go back</button>
     </div>
   );
 };
--- Udemy/JavaScript e TypeScript do básico ao avançado/Seção 16 - React Router Dom v6/src/pages/Home/index.tsx
+++ Udemy/JavaScript e TypeScript do básico ao avançado/Seção 16 - React Router Dom v6/src/pages/Home/index.tsx
@@ -1,7 +1,15 @@
+import { useNavigate } from "react-router-dom";
+
 export const Home = () => {
+  const navigate = useNavigate();
+
+  const handleNavigate = () => {
+    navigate("/about");
+  };
   return (
     <div>
       <h1>Home</h1>
+      <button onClick={handleNavigate}>Navigate</button>
     </div>
   );
 };
