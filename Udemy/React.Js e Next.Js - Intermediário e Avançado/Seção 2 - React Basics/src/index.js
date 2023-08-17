4dbab5e7fdb14e6fd5060ce09ae67e8a396aea2f
Lucas Smaniotto <101435037+lucassmaniotto@users.noreply.github.com>
2023-08-17 11:35:17 -0300

    chore: ajustando arquivos

--- Udemy/React.Js e Next.Js - Intermediário e Avançado/Seção 2 - React Basics/src/index.js
+++ Udemy/React.Js e Next.Js - Intermediário e Avançado/Seção 2 - React Basics/src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App';
 import './styles/global-styles.css';
-
-import { Home } from './templates/Home';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Home />
+    <App />
   </React.StrictMode>,
   document.getElementById('root'),
 );
