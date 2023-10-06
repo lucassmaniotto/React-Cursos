8233ccd972734b9cc38560f52a0f5992eda13115
Lucas Smaniotto <101435037+lucassmaniotto@users.noreply.github.com>
2023-10-06 16:17:02 -0300

    feat: iniciando e publicando projeto com Strapi

---
+++ Udemy/React.Js e Next.Js - Intermediário e Avançado/Seção 7 - Landing Page com Strapi/strapi-landing-pages/types/generated/components.d.ts
@@ -0,0 +1,99 @@
+import type { Schema, Attribute } from '@strapi/strapi';
+
+export interface SectionImageGrid extends Schema.Component {
+  collectionName: 'components_section_image_grids';
+  info: {
+    displayName: 'image-grid';
+    icon: 'dashboard';
+  };
+  attributes: {
+    image: Attribute.Media & Attribute.Required;
+  };
+}
+
+export interface SectionSectionGrid extends Schema.Component {
+  collectionName: 'components_section_section_grids';
+  info: {
+    displayName: 'section_grid';
+    icon: 'grid';
+    description: '';
+  };
+  attributes: {
+    title: Attribute.String & Attribute.Required;
+    description: Attribute.Text & Attribute.Required;
+    text_grid: Attribute.Component<'section.text-grid', true>;
+    image_grid: Attribute.Component<'section.image-grid', true>;
+    metadata: Attribute.Component<'section.section-metadata'> &
+      Attribute.Required;
+  };
+}
+
+export interface SectionSectionMetadata extends Schema.Component {
+  collectionName: 'components_section_section_metadata';
+  info: {
+    displayName: 'section_metadata';
+    icon: 'cube';
+  };
+  attributes: {
+    name: Attribute.String &
+      Attribute.Required &
+      Attribute.SetMinMaxLength<{
+        minLength: 3;
+        maxLength: 255;
+      }>;
+    section_id: Attribute.String &
+      Attribute.Required &
+      Attribute.SetMinMaxLength<{
+        minLength: 3;
+        maxLength: 50;
+      }>;
+    background: Attribute.Boolean &
+      Attribute.Required &
+      Attribute.DefaultTo<false>;
+  };
+}
+
+export interface SectionSectionTwoColumns extends Schema.Component {
+  collectionName: 'components_section_section_two_columns';
+  info: {
+    displayName: 'section_two_columns';
+    icon: 'server';
+    description: '';
+  };
+  attributes: {
+    title: Attribute.String &
+      Attribute.Required &
+      Attribute.SetMinMaxLength<{
+        minLength: 3;
+        maxLength: 255;
+      }>;
+    description: Attribute.Text & Attribute.Required;
+    image: Attribute.Media & Attribute.Required;
+    metadata: Attribute.Component<'section.section-metadata'> &
+      Attribute.Required;
+  };
+}
+
+export interface SectionTextGrid extends Schema.Component {
+  collectionName: 'components_section_text_grids';
+  info: {
+    displayName: 'text-grid';
+    icon: 'bulletList';
+  };
+  attributes: {
+    title: Attribute.String & Attribute.Required;
+    description: Attribute.Text & Attribute.Required;
+  };
+}
+
+declare module '@strapi/types' {
+  export module Shared {
+    export interface Components {
+      'section.image-grid': SectionImageGrid;
+      'section.section-grid': SectionSectionGrid;
+      'section.section-metadata': SectionSectionMetadata;
+      'section.section-two-columns': SectionSectionTwoColumns;
+      'section.text-grid': SectionTextGrid;
+    }
+  }
+}
