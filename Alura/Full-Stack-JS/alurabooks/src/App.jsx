d42ab271c56b9b4332d0dabca91c4e47d75f4608
Lucas Smaniotto <101435037+lucassmaniotto@users.noreply.github.com>
2023-11-17 13:40:00 -0300

    feat: implementando styled components

--- Alura/Full-Stack-JS/alurabooks/src/App.jsx
+++ Alura/Full-Stack-JS/alurabooks/src/App.jsx
@@ -1,12 +1,22 @@
 import { Header } from "./components/Header";
 
-import "./App.css";
+import styled from "styled-components";
+
+const AppContainer = styled.main`
+  width: 100vw;
+  height: 100vh;
+  background-image: linear-gradient(90deg, #002f52 35%, #326589);
+
+  li {
+    list-style: none;
+  }
+`;
 
 function App() {
   return (
-    <div className="App">
+    <AppContainer>
       <Header />
-    </div>
+    </AppContainer>
   );
 }
 
--- Alura/Full-Stack-JS/alurabooks/src/main.jsx
+++ Alura/Full-Stack-JS/alurabooks/src/main.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import GlobalStyles from "./components/GlobalStyles";
+import App from "./App.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <GlobalStyles />
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
