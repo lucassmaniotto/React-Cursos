559382423add4ddc8f252e27e0763947f6b7f490
Lucas Smaniotto <101435037+lucassmaniotto@users.noreply.github.com>
2024-05-09 16:35:48 -0300

    feat: criação de rotas com Next.js

---
+++ Alura/Melhore sua aplicacao React com Next.js/Next.js - explorando o framework/pages/404.js
@@ -0,0 +1,10 @@
+import Link from "../src/components/Link";
+
+export default function Page404() {
+  return (
+    <div>
+      <h1>Parece que essa página não existe... :(</h1>
+      <Link href="/">Voltar para a Home</Link>
+    </div>
+  );
+}
---
+++ Alura/Melhore sua aplicacao React com Next.js/Next.js - explorando o framework/pages/faq.js
@@ -0,0 +1,10 @@
+import Link from "../src/components/Link";
+
+export default function FAQ() {
+  return (
+    <div>
+      <h1>Alura Cases - FAQ</h1>
+      <Link href="/">Voltar para a Home</Link>
+    </div>
+  );
+}
